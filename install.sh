eb2378c04ba4ff8b5752e2b3fcda97082fd0bb78
Randy Adams <r@ngsfed.com>
2015-01-24 20:47:02 -0500

    Better handling of the run-time temp directory.  Can now install from the actual install script.

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-mkdir /var/run/crawl
+DESTDIR=/usr/local/bin
+TMPDIR=/var/run/crawl 
+
+mkdir -m 777 "${TMPDIR}" 2> /dev/null
+install -m 755 -o root -p bin/crawl "${DESTDIR}"
+
+exit $?
